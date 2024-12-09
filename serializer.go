7acea9708808924087611b007143d721f45e9207
Abdelrhman Basyoni <64443028+abdelrhman-basyoni@users.noreply.github.com>
2024-12-09 14:32:08 +0200

    adding the serializers

---
+++ serializer.go
@@ -0,0 +1,57 @@
+package goresp
+
+import (
+	"strconv"
+	"strings"
+)
+
+// serializeCommand converts the given string to RESP formated bytes
+func SerializeCommand(command string) []byte {
+	parts := strings.Fields(command)
+
+	var test []Value
+
+	for _, part := range parts {
+
+		bulk := Value{Typ: "bulk", Bulk: part}
+		test = append(test, bulk)
+	}
+
+	newCommand := Value{Typ: "array", Array: test}
+
+	return newCommand.Marshal()
+}
+
+// SerializeReaderCommand takes resp Value and converts it to string
+func SerializeReaderCommand(res Value) string {
+	//Todo: handle null and error
+	switch res.Typ {
+	case "bulk":
+		{
+			return res.Bulk
+		}
+	case "string":
+		{
+			return res.Str
+		}
+
+	case "int":
+		{
+			return strconv.Itoa(int(res.Num))
+		}
+
+	case "array":
+		{
+			var fullStr string
+			for _, value := range res.Array {
+				fullStr += SerializeReaderCommand(value)
+			}
+
+			return fullStr
+		}
+	default:
+		{
+			return res.Bulk
+		}
+	}
+}
